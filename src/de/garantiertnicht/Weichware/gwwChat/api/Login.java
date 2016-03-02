c99b6518c3466683b87d5d62be134e4686535556
garantiertnicht <garantiertwerbung@me.com>
2016-03-02 20:13:28 +0100

    Fixed Typo

--- src/de/garantiertnicht/Weichware/gwwChat/api/Login.java
+++ src/de/garantiertnicht/Weichware/gwwChat/api/Login.java
@@ -11,8 +11,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *    This product includes software developed by the garantiertnicht Weichware.
- * 4. Neither the name of the garantiertnicht Weichware nor the
+ *    This product includes software developed by garantiertnicht Weichware.
+ * 4. Neither the name of garantiertnicht Weichware nor the
  *    names of its contributors may be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
